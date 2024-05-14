0dfc7e8edac2e18e6cc3e45611629439b6e30e02
saeed <ahmed.m.elsaeed2@gmail.com>
2024-05-14 20:32:26 +0300

    read properties from system.properties

--- src/main/java/server/RMIServer.java
+++ src/main/java/server/RMIServer.java
@@ -2,16 +2,31 @@ package server;
 
 import rmi.GraphObject;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Properties;
 
 public class RMIServer {
     public static void main(String[] args) {
         try {
+            // Load properties
+            InputStream is = RMIServer.class.getClassLoader().getResourceAsStream("system.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+
+            int port = Integer.parseInt(properties.getProperty("GSP.rmiRegistry.port", "1099"));
+            String remoteObjectName = properties.getProperty("GSP.rmiRegistry.remoteObjectName", "RMIServer");
+
+            // Create a Registry instance on localhost that accepts requests on the specified port
+            Registry registry = LocateRegistry.createRegistry(port);
+
+            // Registering a name for the remote object
             GraphObject graphObject = new GraphObject();
-            Registry registry = LocateRegistry.createRegistry(1099);
-            registry.bind("graph", graphObject);
-            System.out.println("Server is ready!");
+            registry.bind(remoteObjectName, graphObject);
+
+            System.out.println("Server is listening on port " + port + " with remote object name " + remoteObjectName);
         }
         catch (Exception e) {
             throw new RuntimeException(e);
