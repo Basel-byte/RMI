36859e563008341624c5848e1c1c6ab4401f266f
Mohamed Ayman <80472863+mo-ayman@users.noreply.github.com>
2024-05-16 10:31:28 +0300

    remove hard password

--- src/main/java/config/SSHExecutor.java
+++ src/main/java/config/SSHExecutor.java
@@ -8,64 +8,8 @@ import java.util.concurrent.CountDownLatch;
 
 public class SSHExecutor {
     public SSHExecutor() {
-
     }
-    public void execute(String user, String host, String command) {
 
-
-        try {
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, 22);
-
-            // Disable strict host key checking
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            session.setConfig(config);
-            session.setPassword("2542001");
-            session.connect();
-
-
-//            command = "source ~/.bashrc && echo $PATH && " + command;
-
-            ChannelExec channel = (ChannelExec) session.openChannel("exec");
-            channel.setCommand(command);
-
-            // Execute command
-            InputStream in = channel.getInputStream();
-            channel.connect();
-
-            byte[] tmp = new byte[1024];
-            while (true) {
-                while (in.available() > 0) {
-                    int i = in.read(tmp, 0, 1024);
-                    if (i < 0) break;
-                    System.out.print(new String(tmp, 0, i));
-                }
-                if (channel.isClosed()) {
-                    System.out.println("exit-status: " + channel.getExitStatus());
-                    break;
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception ee) {
-                }
-            }
-
-            // Check if command execution was successful
-            int exitStatus = channel.getExitStatus();
-            if (exitStatus == 0) {
-                System.out.println("Command executed successfully.");
-            } else {
-                System.err.println("Command execution failed with exit status: " + exitStatus);
-            }
-
-            channel.disconnect();
-            session.disconnect();
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 
     public void executeSingleCommand(String user, String host, String password, String command, CountDownLatch latch) {
         try {
